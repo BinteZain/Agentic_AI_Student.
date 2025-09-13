c3081e0510729f4b3244e326ba3eb85a82235c28
Bint e Zain <bintezain4449@gmail.com>
2025-09-13 22:07:20 +0500

    Add files via upload

---
+++ class 01 may(24).py
@@ -0,0 +1,13 @@
+#  Hello World :
+
+def main():
+    
+    print("Hello from hello-world!")
+
+print("name = " , __name__)
+
+if __name__ == "__main__":
+    
+    main()
+
+# << practiced by Bint e Zain >>
